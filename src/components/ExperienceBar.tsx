68c4111f22e0285042f38c600dee11db2c7d834f
Rodrigo da Silva <74737267+Rodrigo-bs@users.noreply.github.com>
2022-12-13 19:38:23 -0300

    feat: teste da funcionalidade de estados do react, teste a alteração da variável de xp

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
+
 export function ExperienceBar() {
+    const [ xp, setCountXp ] = useState(0);
+
+    function incrementXp() {
+        setCountXp(xp < 100 ? xp + 10 : xp);
+    }
+
+    function decrementXp() {
+        setCountXp(xp > 0 ? xp - 10 : xp);
+    }
+
     return (
         <header className="experience-bar">
+            <button onClick={ decrementXp }>-</button>
+            
             <span>0 xp</span>
             <div>
-                <div style={{ width: '50%' }}></div>
+                <div style={{ width: xp + '%' }}></div>
 
                 <span style={{ left: '50%' }} className="current-experience">
-                    300 xp
+                    { xp * 6 } xp
                 </span>
             </div>
             <span>600 xp</span>
+
+            <button onClick={ incrementXp }>+</button>
         </header>
     );
 }
